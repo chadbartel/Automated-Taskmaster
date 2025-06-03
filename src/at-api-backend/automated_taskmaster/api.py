cf14ce80bd30d29f42b1cbf8e3e4f2436be8349e
thatsmidnight <10793854+thatsmidnight@users.noreply.github.com>
2025-06-03 15:13:38 -0700

    Move documentation endpoints out of separate module

--- src/at-api-backend/automated_taskmaster/api.py
+++ src/at-api-backend/automated_taskmaster/api.py
@@ -5,11 +5,10 @@ import os
 from fastapi import APIRouter
 
 # Local Modules
-from automated_taskmaster.routers import summon, documentation
+from automated_taskmaster.routers import summon
 
 # Create a router instance with a default prefix
 router = APIRouter(prefix=f"/{os.getenv('API_PREFIX', 'api/v1')}")
 
 # Include other routers
 router.include_router(summon.router)
-router.include_router(documentation.router)
