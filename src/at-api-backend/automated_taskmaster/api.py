bb3bae3cc3840bf93d7a0a93b9e33ac9f738aef9
thatsmidnight <10793854+thatsmidnight@users.noreply.github.com>
2025-06-03 15:07:35 -0700

    Add missing router for documentation endpoints

--- src/at-api-backend/automated_taskmaster/api.py
+++ src/at-api-backend/automated_taskmaster/api.py
@@ -5,10 +5,11 @@ import os
 from fastapi import APIRouter
 
 # Local Modules
-from automated_taskmaster.routers import summon
+from automated_taskmaster.routers import summon, documentation
 
 # Create a router instance with a default prefix
 router = APIRouter(prefix=f"/{os.getenv('API_PREFIX', 'api/v1')}")
 
 # Include other routers
 router.include_router(summon.router)
+router.include_router(documentation.router)
